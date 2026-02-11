d96b7696f2f7b1605a943327515c11518104aa4a
munakata-t <munaka.dev@gmail.com>
2026-02-11 10:46:44 +0900

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,17 @@
 // js/firebase-config.js
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAdXQfO5VDJsH84Gw_jdVJPivopuaq0h7M",
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdXQf05VDJsH84Gw_jdVJpivopuaq0h7M",
   authDomain: "mobile-order-app-6f839.firebaseapp.com",
   projectId: "mobile-order-app-6f839",
-  storageBucket: "mobile-order-app-6f839.firebasestorage.app",
+  storageBucket: "mobile-order-app-6f839.appspot.com",
   messagingSenderId: "645504503657",
   appId: "1:645504503657:web:2649f6032b279f873977f8",
-  measurementId: "G-CSZKGRRRCX"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
